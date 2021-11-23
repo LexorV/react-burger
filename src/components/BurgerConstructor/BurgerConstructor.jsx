f6416bdb5dbff4788f0df613456a4f3d3026fbbc
Lex <Lexor.V.A.S@yandex.ru>
2021-11-23 16:10:43 +0300

    add API

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -1,65 +1,72 @@
-import { Typography, Box, CurrencyIcon, DragIcon, ConstructorElement, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import React from 'react';
+import { CurrencyIcon, DragIcon, ConstructorElement, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import './BurgerConstructor.css';
-import { dataIngredients } from '../../utils/data.js';
+//import { dataIngredients } from '../../utils/data.js';
 
-export default function BurgerConstructor() {
-   return (
-      <section className="constructor pt-25 mt-4">
-         <div className="constructor__lock-elements">
-            <ConstructorElement isLocked={true}
-               thumbnail={dataIngredients[0].image} text={dataIngredients[0].name} price={dataIngredients[0].price} />
-         </div>
-         <div className="constructor__open-elements pt-4">
-            <div className="constructor__open-elements-box">
-               <DragIcon type="primary" />
-               <ConstructorElement isLocked={false}
-                  thumbnail={dataIngredients[6].image} text={dataIngredients[6].name} price={dataIngredients[6].price} />
-            </div>
-            <div className="constructor__open-elements-box">
-               <DragIcon type="primary" />
-               <ConstructorElement isLocked={false}
-                  thumbnail={dataIngredients[2].image} text={dataIngredients[2].name} price={dataIngredients[2].price} />
-            </div>
-            <div className="constructor__open-elements-box">
-               <DragIcon type="primary" />
-               <ConstructorElement isLocked={false}
-                  thumbnail={dataIngredients[5].image} text={dataIngredients[5].name} price={dataIngredients[5].price} />
-            </div>
-            <div className="constructor__open-elements-box">
-               <DragIcon type="primary" />
-               <ConstructorElement isLocked={false}
-                  thumbnail={dataIngredients[3].image} text={dataIngredients[3].name} price={dataIngredients[3].price} />
-            </div>
-            <div className="constructor__open-elements-box">
-               <DragIcon type="primary" />
-               <ConstructorElement isLocked={false}
-                  thumbnail={dataIngredients[4].image} text={dataIngredients[4].name} price={dataIngredients[4].price} />
-            </div>
-            <div className="constructor__open-elements-box">
-               <DragIcon type="primary" />
-               <ConstructorElement isLocked={false}
+export default function BurgerConstructor({ dataIngredients }) {
+   if (dataIngredients !== null) {
+      return (
+         <section className="constructor pt-25 mt-4">
+            <div className="constructor__lock-elements">
+               <ConstructorElement isLocked={true}
                   thumbnail={dataIngredients[0].image} text={dataIngredients[0].name} price={dataIngredients[0].price} />
             </div>
-            <div className="constructor__open-elements-box">
-               <DragIcon type="primary" />
-               <ConstructorElement isLocked={false}
+            <div className="constructor__open-elements pt-4">
+               <div className="constructor__open-elements-box">
+                  <DragIcon type="primary" />
+                  <ConstructorElement isLocked={false}
+                     thumbnail={dataIngredients[6].image} text={dataIngredients[6].name} price={dataIngredients[6].price} />
+               </div>
+               <div className="constructor__open-elements-box">
+                  <DragIcon type="primary" />
+                  <ConstructorElement isLocked={false}
+                     thumbnail={dataIngredients[2].image} text={dataIngredients[2].name} price={dataIngredients[2].price} />
+               </div>
+               <div className="constructor__open-elements-box">
+                  <DragIcon type="primary" />
+                  <ConstructorElement isLocked={false}
+                     thumbnail={dataIngredients[5].image} text={dataIngredients[5].name} price={dataIngredients[5].price} />
+               </div>
+               <div className="constructor__open-elements-box">
+                  <DragIcon type="primary" />
+                  <ConstructorElement isLocked={false}
+                     thumbnail={dataIngredients[3].image} text={dataIngredients[3].name} price={dataIngredients[3].price} />
+               </div>
+               <div className="constructor__open-elements-box">
+                  <DragIcon type="primary" />
+                  <ConstructorElement isLocked={false}
+                     thumbnail={dataIngredients[4].image} text={dataIngredients[4].name} price={dataIngredients[4].price} />
+               </div>
+               <div className="constructor__open-elements-box">
+                  <DragIcon type="primary" />
+                  <ConstructorElement isLocked={false}
+                     thumbnail={dataIngredients[0].image} text={dataIngredients[0].name} price={dataIngredients[0].price} />
+               </div>
+               <div className="constructor__open-elements-box">
+                  <DragIcon type="primary" />
+                  <ConstructorElement isLocked={false}
+                     thumbnail={dataIngredients[0].image} text={dataIngredients[0].name} price={dataIngredients[0].price} />
+               </div>
+            </div>
+            <div className="constructor__lock-elements mt-4">
+               <ConstructorElement isLocked={true}
                   thumbnail={dataIngredients[0].image} text={dataIngredients[0].name} price={dataIngredients[0].price} />
             </div>
-         </div>
-         <div className="constructor__lock-elements mt-4">
-            <ConstructorElement isLocked={true}
-               thumbnail={dataIngredients[0].image} text={dataIngredients[0].name} price={dataIngredients[0].price} />
-         </div>
-         <div className="constructor__price-container mt-10 mr-4">
-            <div className="constructor__price-text mr-10">
-               <p className="text text_type_digits-medium pr-2"> 400</p>
-               <CurrencyIcon type="primary" />
+            <div className="constructor__price-container mt-10 mr-4">
+               <div className="constructor__price-text mr-10">
+                  <p className="text text_type_digits-medium pr-2"> 400</p>
+                  <CurrencyIcon type="primary" />
+               </div>
+               <Button type="primary" size="large">
+                  Оформить заказ
+               </Button>
             </div>
-            <Button type="primary" size="large">
-               Оформить заказ
-            </Button>
-         </div>
-
-      </section>
-   )
+         </section>
+      )
+   }
+   else {
+      return (
+         <h2> Загрузка...</h2>
+      )
+   }
 }
--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import {
-    Typography,
-    Box,
     CurrencyIcon,
     Tab,
-    ConstructorElement,
     Counter
 } from '@ya.praktikum/react-developer-burger-ui-components';
 import './BurgerIngredients.css';
-import {dataIngredients} from '../../utils/data.js';
-const Ingtrdients = ({data, type}) => {
-    const listItems = data
-        .filter(e => e.type === type)
-        .map((element) => <li key={element._id} className="ingredients__card-list pl-4">
-            <Counter count={1} size="default"/>
-            <img alt={element.name} src={element.image} className="ingredients__picture pl-4 pr-4 "></img>
-            <div className="ingredients__card-price-box pt-1 pb-1">
-                <p className="text text_type_digits-default pr-2">{element.price}</p>
-                <CurrencyIcon type="primary"/>
-            </div>
-            <p className="text text_type_main-small">{element.name}</p>
-        </li>)
-    return listItems
+//import {dataIngredients} from '../../utils/data.js';
+const Ingtrdients = ({ data, type }) => {
+    if (data !== null) {
+        const listItems = data
+            .filter(e => e.type === type)
+            .map((element) => <li key={element._id} className="ingredients__card-list pl-4">
+                <Counter count={1} size="default" />
+                <img alt={element.name} src={element.image} className="ingredients__picture pl-4 pr-4 "></img>
+                <div className="ingredients__card-price-box pt-1 pb-1">
+                    <p className="text text_type_digits-default pr-2">{element.price}</p>
+                    <CurrencyIcon type="primary" />
+                </div>
+                <p className="text text_type_main-small">{element.name}</p>
+            </li>)
+        return listItems
+    }
+    else {
+        return (
+            <h2>Загрузка</h2>)
+    }
 }
-export default function BurgerIngredients() {
-    const [dataIngrid, setDataIngrid] = React.useState(dataIngredients)
+export default function BurgerIngredients({ dataIngrid }) {
+    //  const [dataIngrid, setDataIngrid] = React.useState(dataIngredients)
     const [current, setCurrent] = React.useState('Булки')
     return (
         <section className="ingredients">
@@ -33,8 +36,8 @@ export default function BurgerIngredients() {
             <div
                 className="mt-10 mb-5"
                 style={{
-                display: 'flex'
-            }}>
+                    display: 'flex'
+                }}>
                 <Tab active={current === 'Булки'} value="Булки" >Булки</Tab>
                 <Tab value="Соусы">Соусы</Tab>
                 <Tab value="Начинки">Начинки</Tab>
@@ -48,11 +51,11 @@ export default function BurgerIngredients() {
                 </ul>
                 <h2 className="text text_type_main-medium">Соусы</h2>
                 <ul className="ingredients__lists pl-2">
-                <Ingtrdients data={dataIngrid} type='sauce' />
+                    <Ingtrdients data={dataIngrid} type='sauce' />
                 </ul>
                 <h2 className="text text_type_main-medium">Начинки</h2>
                 <ul className="ingredients__lists pl-2">
-                <Ingtrdients data={dataIngrid} type='main' />
+                    <Ingtrdients data={dataIngrid} type='main' />
                 </ul>
             </div>
 
