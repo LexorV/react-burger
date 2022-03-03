9b2e4bf16f3670a9a8d2938ffca6e9b9f43a47fc
Lex <Lexor.V.A.S@yandex.ru>
2022-03-03 04:48:58 +0300

    fix reset and forgot

--- src/services/reducers/registerForm.ts
+++ src/services/reducers/registerForm.ts
@@ -6,20 +6,28 @@ import {
     LOGOUT_USER,
     fORGOT_FORM_SUBMIT,
     fORGOT_FORM_SUCCESS,
-    fORGOT_FORM_FAILED
+    fORGOT_FORM_FAILED,
+    fORGOT_FORM_CLEANING,
+    RESET_FORM_SUBMIT,
+    RESET_fORM_SUCCESS,
+    RESET_fORM_FAILED,
+    RESET_fORM_CLEANING
 } from '../action/registerForm'
 const initialState = {
     email: '',
     password: '',
     name: '',
-    emailForgot:'',
+    emailForgot: '',
     emailCode: '',
     registrationRequest: false,
     registrationFailed: false,
     registrationSuccess: false,
     registerReceivedData: null,
     resetSuccess: false,
-    resetFailed: false
+    resetFailed: false,
+    forgotSuccess: false,
+    forgotFailed: false,
+
 }
 export const RegistrationReduser = (state = initialState, action: any) => {
     switch (action.type) {
@@ -71,26 +79,67 @@ export const RegistrationReduser = (state = initialState, action: any) => {
             return {
                 ...state,
                 registrationRequest: true,
-                resetSuccess: false,
-                resetFailed: false
+                forgotSuccess: false,
+                forgotFailed: false
             }
         }
         case fORGOT_FORM_SUCCESS: {
             return {
                 ...state,
                 registrationRequest: false,
-                resetSuccess: true,
-                resetFailed: false
+                forgotSuccess: true,
+                forgotFailed: false
             }
         }
         case fORGOT_FORM_FAILED: {
             return {
                 ...state,
                 registrationRequest: false,
-                resetSuccess: false,
-                resetFailed: true
+                forgotSuccess: false,
+                forgotFailed: true
             }
         }
+        case fORGOT_FORM_CLEANING: {
+            return {
+                ...state,
+                emailForgot: '',
+                emailCode: '',
+            }
+        }
+        case RESET_FORM_SUBMIT:{
+            return {
+                ...state,
+                registrationRequest: true,
+                resetSuccess: false,
+                resetFailed: false
+            }
+        }
+            case RESET_fORM_SUCCESS: {
+                return {
+                    ...state,
+                    registrationRequest: false,
+                    resetSuccess: true,
+                    resetFailed: false
+                }
+            }
+            case RESET_fORM_FAILED: {
+                return {
+                    ...state,
+                    registrationRequest: false,
+                    resetSuccess: false,
+                    resetFailed: true
+                }
+            }
+            case RESET_fORM_CLEANING: {
+                return {
+                    ...state,
+                    registrationRequest: false,
+                    resetSuccess: false,
+                    resetFailed: false,
+                    password: '',
+                    emailCode:''
+                }
+            }
         default: {
             return state;
         }
