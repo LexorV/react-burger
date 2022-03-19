66cd035212c398fc6e9b08e86e16512a65a4ea59
Lex <Lexor.V.A.S@yandex.ru>
2022-03-19 03:10:31 +0300

    add feed logics part_2

--- src/components/App/pages/feed/feedOrderCard.tsx
+++ src/components/App/pages/feed/feedOrderCard.tsx
@@ -4,17 +4,37 @@ import { useSelector, useDispatch } from '../../../../services/hooks';
 import {
     CurrencyIcon
 } from '@ya.praktikum/react-developer-burger-ui-components';
+const ListPicture = ({pictureArray}:any) => {
+    if (pictureArray.length > 0) {
+        const test = pictureArray.map((el: any) =>
+        <img className={feedStyle.card__picture} style={{zIndex:8}} alt={el.name} src={el.image}></img>
+        ).slice(0, 5);
+        return test
+    }
+    else {
+        return (
+            <p>Странно, бургеров в заказе нет</p>
+        )
+    }
+}
+
+
 export const  FeedOrderCard = ({ordesData}:any) =>{
     const { ingredients } = useSelector(state => state.ingredients);
+    const [ingredientPictureArray, setIngredientPictureArray] = useState<any>([])
     const addIngredientsOrder = () => {
         let ordersIngredient
         if (ingredients) {
-            console.log(ingredients)
             ordersIngredient = ingredients.filter(el => ordesData.ingredients.includes(el._id))
-            console.log(ordersIngredient)
-            return ordersIngredient
+            //console.log(ordersIngredient)
+            setIngredientPictureArray(ordersIngredient)
         }
+        return ordersIngredient
     }
+    useEffect(() => {
+        addIngredientsOrder()
+    },[ingredients, ordesData])
+
 return (
     <li className={feedStyle.card}>
         <div className={feedStyle.card__number_box}>
@@ -23,10 +43,12 @@ return (
         </div>
         <h2 className='text text_type_main-medium pb-6'>{ordesData.name}</h2>
         <div className={feedStyle.card__bottom_box}>
-            <div>
-                <img className={feedStyle.card__picture} style={{zIndex:8}} alt="Картинка" src="https://code.s3.yandex.net/react/code/sp_1.png"></img>
-                <img className={feedStyle.card__picture} style={{zIndex:7}} alt="Картинка" src="https://code.s3.yandex.net/react/code/bun-01.png"></img>
-                <img className={feedStyle.card__picture} alt="Картинка" src="https://code.s3.yandex.net/react/code/bun-01.png"></img>
+            <div className={feedStyle.card__picture_box} >
+                <ListPicture pictureArray= {ingredientPictureArray} />
+                {ingredientPictureArray[6] && (<div className={feedStyle.card_picture_number_box}>
+                <img className={`${feedStyle.card__picture} ${feedStyle.card_picture_last}`}
+                style={{zIndex:8}} alt={ingredientPictureArray[6].name} 
+                src={ingredientPictureArray[6].image}></img><p className={feedStyle.card_picture_number_text}>+2</p></div>)}
             </div>
             <div className={feedStyle.card__price_box}>
                 <p className={`${feedStyle.card__price} text text_type_digits-default `}>340</p>
