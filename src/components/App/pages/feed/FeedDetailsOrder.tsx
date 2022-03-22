72cc6276d614bfc5e120c0224bbe34428d42ddcf
Lex <Lexor.V.A.S@yandex.ru>
2022-03-22 03:08:14 +0300

    layout feed/id

--- src/components/App/pages/feed/FeedDetailsOrder.tsx
+++ src/components/App/pages/feed/FeedDetailsOrder.tsx
@@ -1,5 +1,33 @@
+import feedStyle from './feed.module.css';
+import {
+    CurrencyIcon
+} from '@ya.praktikum/react-developer-burger-ui-components';
 export const FeedDetailsOrder = () => {
     return (
-        <div>Test Order</div>
+        <section className={feedStyle.details_main}>
+        <div  className={feedStyle.details_main_popap}>
+            <h2 className={`text_type_main-small mb-10 ${feedStyle.details_number_order}`}>#034533</h2>
+            <p className='text text_type_main-medium mb-3'>Black Hole Singularity острый бургер</p>
+            <p className='text text_type_main-small mb-15'>Выполнен</p>
+            <p className ='text text_type_main-medium mb-6'>Cостав:</p>
+            <ul className={feedStyle.details_lists}>
+                <li className = {feedStyle.details_list_ingredient}>
+                    <img className={`${feedStyle.card__picture} mr-4`} alt='картинка'  src='https://code.s3.yandex.net/react/code/meat-01.png'></img>
+                    <p className='text text_type_main-small'>Флюоресцентная булка R2-D3</p>
+                    <div className={feedStyle.details_ingredient_price_box}>
+                        <p> 2 x 20</p>
+                        <CurrencyIcon type="primary" />
+                    </div>
+                </li>
+            </ul>
+            <div className={feedStyle.details_bottom_box}>
+                <p className='text_color_inactive'>Вчера, 13:50 i-GMT+3</p>
+                <div className ={feedStyle.details_box_price}>
+                <p className ='text text_type_digits-default'>510</p>
+                <CurrencyIcon type="primary" />
+                </div>
+            </div>
+        </div>
+        </section>
     )
 }
--- src/components/App/pages/feed/feedOrderStatistics.tsx
+++ src/components/App/pages/feed/feedOrderStatistics.tsx
@@ -1,13 +1,13 @@
 import feedStyle from './feed.module.css';
-import { useSelector} from '../../../../services/hooks';
+import { useSelector } from '../../../../services/hooks';
 import { FC, useState, useEffect } from 'react';
-const OrdesDoneList:any = () => {
+const OrdesDoneList: any = () => {
     const { orders } = useSelector(state => state.wsOrdes);
     const [ordersTemp, setOrdesTemp] = useState([]);
     const filterDoneOrder = () => {
-        if(orders !== null) {
-            const tempOrder = orders.orders.filter((el:any) => el.status === 'done')
-            .slice(0, 5).map((el:any) => el.number)
+        if (orders !== null) {
+            const tempOrder = orders.orders.filter((el: any) => el.status === 'done')
+                .slice(0, 5).map((el: any) => el.number)
             setOrdesTemp(tempOrder)
         }
     }
@@ -15,55 +15,55 @@ const OrdesDoneList:any = () => {
         filterDoneOrder()
         console.log(ordersTemp)
     }, [orders])
-    if(ordersTemp.length > 0) {
+    if (ordersTemp.length > 0) {
         const temp = ordersTemp.map((el: any) =>
-        <li className={`text text_type_digits-default ${feedStyle.statistics__number}`}>{el}</li>
-    )
-    return temp
-    }
-        return (
-            <li className={'text text_type_main-small'}>Готовых заказов нет</li>
+            <li className={`text text_type_digits-default ${feedStyle.statistics__number}`}>{el}</li>
         )
+        return temp
     }
-    const OrdesNotDoneList :any = () => {
-        const { orders } = useSelector(state => state.wsOrdes);
-        const [ordersTemp, setOrdesTemp] = useState([]);
-        const filterDoneOrder = () => {
-            if(orders !== null) {
-                const tempOrder = orders.orders.filter((el:any) => el.status === 'pending')
-                .slice(0, 5).map((el:any) => el.number)
-                setOrdesTemp(tempOrder)
-            }
+    return (
+        <li className={'text text_type_main-small'}>Готовых заказов нет</li>
+    )
+}
+const OrdesNotDoneList: any = () => {
+    const { orders } = useSelector(state => state.wsOrdes);
+    const [ordersTemp, setOrdesTemp] = useState([]);
+    const filterDoneOrder = () => {
+        if (orders !== null) {
+            const tempOrder = orders.orders.filter((el: any) => el.status === 'pending')
+                .slice(0, 5).map((el: any) => el.number)
+            setOrdesTemp(tempOrder)
         }
-        useEffect(() => {
-            filterDoneOrder()
-            console.log(ordersTemp)
-        }, [orders])
-        if(ordersTemp.length > 0) {
-            const temp = ordersTemp.map((el: any) =>
+    }
+    useEffect(() => {
+        filterDoneOrder()
+        console.log(ordersTemp)
+    }, [orders])
+    if (ordersTemp.length > 0) {
+        const temp = ordersTemp.map((el: any) =>
             <li className={'text text_type_digits-default'}>{el}</li>
         )
         return temp
-        }
-            return (
-                <li className={'text text_type_main-small'}>Все заказы готовы</li>
-            )
-        }
-    
+    }
+    return (
+        <li className={'text text_type_main-small'}>Все заказы готовы</li>
+    )
+}
+
 
 
 
 export const FeedOrderStatistics = () => {
     const { orders } = useSelector(state => state.wsOrdes);
 
-   // console.log(orders)
+    // console.log(orders)
     return (
         <div className={feedStyle.statistics}>
             <div className={feedStyle.statistics__number_order}>
                 <div className={feedStyle.statistics__number_box}>
                     <p className='text text_type_main-medium mb-6'>Готовы:</p>
                     <ul className={feedStyle.statistics__number_list}>
-                    <OrdesDoneList />
+                        <OrdesDoneList />
                     </ul>
                 </div>
                 <div className={feedStyle.statistics__number_box}>
